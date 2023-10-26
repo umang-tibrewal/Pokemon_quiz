05ab887ee389c994b7f80048cdc752bb85c919a4
umang-tibrewal <87933066+umang-tibrewal@users.noreply.github.com>
2023-10-27 01:33:17 +0530

    keep going

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+//list of 24 random pokemon
+
 const popularPokemon = [
     "pikachu",
     "charizard",
@@ -24,22 +26,60 @@ const popularPokemon = [
     "venusaur",
     "alakazam"
   ]
-  let url
+  
  
 
-
+//selecting my html 
 let img=document.querySelector("#pokemon-image")
-
 let genarate=document.querySelector("#generate-button")
 
+//function to generate ramdon pokemon name to genrate image 
 
+let pokemonName=function(){
+  let randomvalue= Math.floor(Math.random()*23)
+
+   let  pokemon= popularPokemon[randomvalue];
+
+   return pokemon
+
+}
+//link with pokemon name to generate pokemon image 
+let link =function(pokemon){
+
+  const base="https://pokeapi.co/api/v2/pokemon/"
+  return base+pokemon
+}
+
+function checkAnswer(pokemon) {
+  let submitHandler = (e) => {
+    e.preventDefault();
+    const answer = document.querySelector("#answer-input").value;
+    if (answer.toLowerCase() === pokemon) {
+      console.log("true answer");
+    } else {
+      console.log("false answer");
+    }
+  }
+
+  form=document.querySelector("form")
+  
+  form.removeEventListener("submit", submitHandler); // Remove the previous event listener
+  form.addEventListener("submit", submitHandler); // Add a new event listener
+
+}
+
+
+
+let pokemon
+// genrate button ->call link by fetch
 genarate.addEventListener("click",(e)=>{
-    let randomvalue= Math.floor(Math.random()*23)
+  
+ pokemon=pokemonName()
+console.log(`${pokemon}`)
 
-   let  pokemonName= popularPokemon[randomvalue];
-
-    const base= "https://pokeapi.co/api/v2/pokemon/"
-    const api=base+pokemonName
+let api=link(pokemon)
+    
+    
     fetch(api)
   .then((data)=>{
          return data.json()
@@ -48,17 +88,24 @@ genarate.addEventListener("click",(e)=>{
   
       url=gd.sprites.other.dream_world.front_default
       img.setAttribute("src",url)
-      
+   
    
   }).catch((error)=>{
   
-      console.log("error");
+      console.log(`error: ${error}`);
   })
 
+  
+  checkAnswer(pokemon)
+  console.log(pokemon)
+
+  // document.querySelector("#answer-input").value="write answer"
   
   
 
 
 })
+
+
 
 
